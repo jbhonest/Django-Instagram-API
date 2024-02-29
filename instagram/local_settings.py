93a673fe784d016bfa891a535d9b0eaa8bf8e190
jbhonest <jbhonest@live.com>
2024-03-01 03:11:47 +0330

    first commit

---
+++ instagram/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xtiu#ca=9f5)6-f2vmu8n1!30ec-1$lxuxk_cfc61+iq0_*)0('
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Define admin URL
+ADMIN_URL = 'myadmin/'
---
+++ instagram/sample_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Define admin URL
+ADMIN_URL = 'admin/'
